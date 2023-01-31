a51e61d5efb5a42061c51882f33e242e8fdcf7a6
Arkadiusz <Arkadiusz@LAPTOP-RJJ77HA0>
2023-01-31 23:34:09 +0100

    Add project files.

---
+++ Cwiczenie 11/Data/Cwiczenie_11Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cwiczenie_11.Models;
+
+namespace Cwiczenie_11.Data
+{
+    public class Cwiczenie_11Context : DbContext
+    {
+        public Cwiczenie_11Context (DbContextOptions<Cwiczenie_11Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cwiczenie_11.Models.Movie> Movie { get; set; }
+    }
+}
